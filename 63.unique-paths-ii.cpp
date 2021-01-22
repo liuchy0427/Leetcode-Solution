7e4670267af751c71a79d59c7ec14a8b3bea4d14
Joey Liu <56081127+liuchy0427@users.noreply.github.com>
2021-01-22 19:46:46 +0800

    2020.01.22

--- 63.unique-paths-ii.cpp
+++ 63.unique-paths-ii.cpp
@@ -10,27 +10,45 @@ public:
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         int m = obstacleGrid.size();
         int n = obstacleGrid[0].size();
-        if(n == 1)  return 1;
-        if(m == 1)  return 1;
-        vector<vector<int>> dp(m , vector<int>(n, 1));
-        for(int i=0;i<2;i++){
-            for(int j=0;j<2;j++){
-                if(obstacleGrid[i][j] == 1){
-                    dp[i][j] = 0;
-                }
-            }
+    
+        vector<int> pre(m, 0);
+        vector<int> cur(m, 0);
+
+        for(int i=0;i<m;i++){
+            if(obstacleGrid[i][0] == 0)
+                pre[i] = 1;
+            else
+                break;
         }
         
-        for(int i=1;i<m;i++){
-            for(int j=1;j<n;j++){
-                if(obstacleGrid[i][j] == 1)
-                    dp[i][j] = 0;
-                else
-                    dp[i][j] = dp[i-1][j] + dp[i][j-1];
+        for(int j=1;j<n;j++){
+            bool flag = false;
+            if(obstacleGrid[0][j] == 0){
+                cur[0] = pre[0];
+                if(cur[0] == 1)
+                    flag = true;
             }
+            else
+                cur[0] = 0;
+            
+            for(int i=1;i<m;i++){
+                if(obstacleGrid[i][j] == 1){
+                    cur[i] = cur[i-1] + pre[i];
+                    if(cur[i] == 1)
+                        flag = true;
+                }
+                else
+                    cur[i] = 0;
+            }
+
+            if(flag == 0)
+                return 0;
+            swap(pre, cur);
         }
 
-        return dp[m-1][n-1];
+        return pre[m-1];
+
+        
     }
 };
 // @lc code=end
