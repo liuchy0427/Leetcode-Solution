96823f1a211ccfac8dc66b07ef52ebcb04f49757
Joey Liu <56081127+liuchy0427@users.noreply.github.com>
2021-02-24 13:43:22 +0800

    2020.02.23

--- 1104.path-in-zigzag-labelled-binary-tree.cpp
+++ 1104.path-in-zigzag-labelled-binary-tree.cpp
@@ -8,7 +8,18 @@
 class Solution {
 public:
     vector<int> pathInZigZagTree(int label) {
-        
+        int level = log(label) / log(2) + 1;
+
+        vector<int> path(level);
+
+        while(label){
+            path[level - 1] = label;
+            label = pow(2, level) - 1 - label + pow(2, level - 1);
+            label >>= 2;
+            level--;
+        }
+
+        return path;
     }
 };
 // @lc code=end
